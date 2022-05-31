eb51c7b421e876e708787e51f3fd835e02243387
Julius Kanani <juliusm.kanani@gmail.com>
2022-05-31 17:53:27 +0300

    task 10, Technical interview preparation. a function in c that checks if a singly linked list has a cycle in it

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,17 +9,15 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *head = list, *current_node = NULL;
+	listint_t *head = list, *current_node = list;
 
 	while (head != NULL)	/* if head exists */
 	{
-		current_node = head;	/* head is the current node */
-		while (current_node != NULL)
+		while (current_node->next != NULL)
 		{
-			/* if current node points to the head */
+			/* does the current node point to itself */
 			if (current_node->next == head)
 				return (1);
-			/* if it does not move to the next node */
 			current_node = current_node->next;
 		}
 
