f683e7ed38b110b6d96fa4acfd76d36cce2ba7ba
Julius Kanani <juliusm.kanani@gmail.com>
2022-06-02 22:20:33 +0300

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+from sys import argv
 if __name__ == "__main__":
-    from sys import argv
-    add = 0
-    for s in argv[1:]:
-        add += int(s)
-    print("{:d}".format(add))
+    result = 0
+    for i in range(1, len(argv)):
+        result += int(argv[i])
+    print('{:d}'.format(result))
