df33c32e0c9d9ea27a0d360877e4fb67133668cc
Julius Kanani <juliusm.kanani@gmail.com>
2022-06-03 05:22:26 +0300

    Task 5 - Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+from variable_load_5 import a
+
+
+def print_variable():
+    print(a)
+
+
 if __name__ == "__main__":
-    from variable_load_5 import a
-    print("{:d}".format(a))
+    print_variable()
