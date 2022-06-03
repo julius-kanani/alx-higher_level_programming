84e87d30e85e939c43cbdb059052f02412e9f7e2
Julius Kanani <juliusm.kanani@gmail.com>
2022-06-03 05:16:28 +0300

    Task 3 - fixed failed correct output errors.

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
+
+
+def infinite_addition():
+    infinite_sum = 0
+    ac = len(argv)
+
+    for index, args in enumerate(argv):
+        if index == 0:
+            continue
+        infinite_sum += int(args)
+    print("{:d}".format(infinite_sum))
+
+
 if __name__ == "__main__":
-    result = 0
-    for i in range(1, len(argv)):
-        result += int(argv[i])
-    print('{:d}'.format(result))
+    infinite_addition()
