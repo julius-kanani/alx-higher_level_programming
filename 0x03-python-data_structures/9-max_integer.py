019a5bd3e0976c20462cbb74125f64df888dde07
Julius Kanani <juliusm.kanani@gmail.com>
2022-06-06 20:41:14 +0300

    Task 9 - Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    """Finds the biggest integer of a list
+
+    Args:
+        my_list: List to look for its biggest integer.
+
+    Returns:
+        The return value. biggest integer
+        if list is empty, return None
+    """
+
+    max_int = 0
+
+    if my_list:
+        for integer in my_list:
+            if integer > max_int:
+                max_int = integer
+
+        return (max_int)
+
+    return (None)
+
+
+if __name__ == "__main__":
+    max_integer(my_list)
