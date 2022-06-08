6ab59c95de51f43d98b91bd13340bdbda9f17a5b
Julius Kanani <juliusm.kanani@gmail.com>
2022-06-08 13:43:31 +0300

    Task 3 - Present in both sets.

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    """Returns a set of common elements in two sets.
+
+    Args:
+        set_1: The first set.
+        set_2: The second set.
+
+    Returns:
+        The return value. A set of common elements in the two sets.
+    """
+
+    if (len(set_1) > 0) and (len(set_2) > 0):
+        common_set = set()
+        for element in set_1:
+            if element in set_2:
+                common_set.add(element)
+        return (common_set)
+
+    return (None)
+
+
+if __name__ == "__main__":
+    common_elements(set_1, set_2)
