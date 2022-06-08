841793f9da6df5e2ea461229ae3aa63efe08db42
Julius Kanani <juliusm.kanani@gmail.com>
2022-06-08 15:02:27 +0300

    Task 5 - a function that returns the numbers of keys in a dictionary

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -11,14 +11,8 @@ def common_elements(set_1, set_2):
     Returns:
         The return value. A set of common elements in the two sets.
     """
-    common_set = set()
 
-    if (len(set_1) > 0) and (len(set_2) > 0):
-        for element in set_1:
-            if element in set_2:
-                common_set.add(element)
-
-    return (common_set)
+    return (set_1 & set_2)
 
 
 if __name__ == "__main__":
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary.
+
+    Args:
+        a_dictionary: The dictionary to find its number of keys.
+
+    Returns:
+        The return value: number of keys.
+    """
+
+    return (len(a_dictionary.keys()))
+
+
+if __name__ == "__main__":
+    number_keys(a_dictionary)
