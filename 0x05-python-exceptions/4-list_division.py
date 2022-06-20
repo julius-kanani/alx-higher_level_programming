9f702dacd0372c461c5bde8d2c7c10e9b98e19aa
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-20 16:34:24 +0300

    Divide a list,

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,7 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
         The return value. A new list(length = list_length) with all divisions
     """
 
-    result = [0] * list_length
+    result = [] 
     divide = 0
 
     for i in range(list_length):
@@ -28,9 +28,8 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            if divide != 0:
-                result[i] = divide
-                divide = 0
+            result.append(divide)
+            divide = 0
 
     return (result)
 
