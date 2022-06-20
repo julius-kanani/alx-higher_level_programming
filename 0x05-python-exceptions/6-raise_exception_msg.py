7c6a843f6a9672b0d7cfa2e647525c78b7c514d4
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-20 16:54:39 +0300

    task 6 - raises an exception with a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def raise_exception_msg(message=""):
+    """Raise a name exception with a name message
+
+    Args:
+        message: The message to display.
+
+    Returns:
+        The return value. Nothing.
+    """
+
+    raise NameError(message)
+
+
+if __name__ == "__main__":
+    raise_exceptioni_msg(message)
