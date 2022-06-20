9878c87bc3db2aa1beb6985fbf93f89594b2c5fa
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-20 16:16:30 +0300

    Task 4 - Divide a list.

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides element by element in 2 lists
+
+    Args:
+        my_list_1: The first list.
+        my_list_2: The second list.
+        list_length: length of list to be returned.
+
+    Returns:
+        The return value. A new list(length = list_length) with all divisions
+    """
+
+    result = [0] * list_length
+    divide = 0
+
+    for i in range(list_length):
+        try:
+            divide = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            continue
+        except TypeError:
+            print("wrong type")
+            continue
+        except IndexError:
+            print("out of range")
+        finally:
+            if divide != 0:
+                result[i] = divide
+                divide = 0
+
+    return (result)
+
+
+if __name__ == "__main__":
+    list_division(my_list_1, my_list_1, list_length)
