9c19262b9873293fd76dcb0b14973394cbe15fb7
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-20 16:41:44 +0300

    Task 5 - raise a type error exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def raise_exception():
+    """Raise a type exception
+
+    Args:
+        No args.
+
+    Returns:
+        The return value. Nothing.
+    """
+
+    raise TypeError
+
+
+if __name__ == "__main__":
+    raise_exception()
