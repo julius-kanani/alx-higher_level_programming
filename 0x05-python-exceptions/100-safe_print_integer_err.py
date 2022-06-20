d3f37365ffb6222c29e7fa780d4f497ab289d5e7
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-20 21:36:46 +0300

    Task 7 - Safe integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    """Prints an integer
+
+    Args:
+        value: The integer to be printed.
+
+    Returns:
+        The return value. True if value printed correctly,
+        False, if value is not an integer.
+    """
+
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except ValueError as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+
+    return (False)
+
+
+if __name__ == "__main__":
+    safe_print_integer_err(value)
