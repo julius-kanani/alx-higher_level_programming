e4aaa9c80f7a24f749c7dfa621ae71bf35485e28
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-20 16:36:24 +0300

    task 4 - fixed pycodestyle errors, trailing whitespace

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,7 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
         The return value. A new list(length = list_length) with all divisions
     """
 
-    result = [] 
+    result = []
     divide = 0
 
     for i in range(list_length):
