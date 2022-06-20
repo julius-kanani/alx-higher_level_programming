c847d40cb787064b0355462f6d6131a1566258a9
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-20 13:06:11 +0300

    Task 1 - handled type error and value error errors

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -14,7 +14,7 @@ def safe_print_integer(value):
 
     try:
         print("{:d}". format(value))
-    except Exception as Error:
+    except (ValueError, TypeError):
         return (False)
     else:
         return (True)
