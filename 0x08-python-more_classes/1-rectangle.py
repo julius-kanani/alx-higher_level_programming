d8dbafc810cce8989835f03d6f97fa4f1eda98f1
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-27 21:36:26 +0300

    Task 1 - handled if value is not none

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -42,13 +42,14 @@ class Rectangle:
         Returns:
             None.
         """
-        if type(value) is int:
-            if value >= 0:
-                self.__width = value
+        if value:
+            if type(value) is int:
+                if value >= 0:
+                    self.__width = value
+                else:
+                    raise ValueError("width must be >= 0")
             else:
-                raise ValueError("width must be >= 0")
-        else:
-            raise TypeError("width must be an integer")
+                raise TypeError("width must be an integer")
 
     @property
     def height(self):
@@ -67,10 +68,11 @@ class Rectangle:
             None.
         """
 
-        if type(value) is int:
-            if value >= 0:
-                self.__height = value
+        if value:
+            if type(value) is int:
+                if value >= 0:
+                    self.__height = value
+                else:
+                    raise ValueError("height must be >= 0")
             else:
-                raise ValueError("height must be >= 0")
-        else:
-            raise TypeError("height must be an integer")
+                raise TypeError("height must be an integer")
