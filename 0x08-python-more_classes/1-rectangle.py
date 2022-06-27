9251e773276da613dd9b85dcfc2cafa5784fa90a
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-27 20:48:27 +0300

    Task 1 - Real definition of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+""" Module 0-rectangle
+
+This module contains the Rectangle class.
+"""
+
+
+class Rectangle:
+    """ Defines a Rectangle
+
+    Attributes:
+        __width (int): The width size of the rectangle.
+        __height (int): The height of the rectangle.
+
+    """
+
+    def __init__(self, width=0, height=0):
+        """ Initializes the Rectangle
+
+        Args:
+            width (int): The width size of a given rectangle.
+            height (int): The height size of a given rectangle.
+
+        """
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ Returns the width of a given rectangle """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Sets the width value of a given rectangle
+
+        Args:
+            value (int): The value to set as width.
+
+        Returns:
+            None.
+        """
+        if type(value) is int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        """ Returns the height of a given rectangle. """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Sets the height value of a given rectangle
+
+        Args:
+            value (int): The value to set as height.
+
+        Returns:
+            None.
+        """
+
+        if type(value) is int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
