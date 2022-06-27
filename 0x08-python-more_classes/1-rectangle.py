cdd83138ccd09b281d0784ea11eb952df9b14184
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-27 21:45:28 +0300

    task 1 - removed underscores.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,8 +23,8 @@ class Rectangle:
 
         """
 
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
