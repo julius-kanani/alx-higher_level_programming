e8b47b8800e51076bb6155a00e2cf30e2453fdbc
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-07-06 00:08:40 +0300

    Task 10 - fixed pycodestyle errros

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -21,6 +21,6 @@ class Student:
         for a in attrs:
             try:
                 new_dict[a] = self.__dict__[a]
-            except:
+            except Exception as Error:
                 pass
         return new_dict
