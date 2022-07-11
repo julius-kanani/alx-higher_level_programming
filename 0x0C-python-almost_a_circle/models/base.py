35fa81bf4aa33f22b7d2dcea7127d3b87b70c95b
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-07-11 20:18:04 +0300

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -52,9 +52,8 @@ class Base:
             list_dictionaries: Dictionary representations of instances."
         """
 
-        if (list_dictionaries is None) or (len(list_dictionaries) == 0):
+        if (list_dictionaries is None) or list_dictionaries == []:
             return "[]"
-
         return json.dumps(list_dictionaries)
 
     @classmethod
