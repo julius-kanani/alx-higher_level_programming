302f6a602d0ecd4a29682f23b7375b400d9e80b8
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-07-11 20:42:38 +0300

    Test cases for base class

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -23,7 +23,7 @@ class TestBase(unittest.TestCase):
 
     def test_type_id(self):
         """ Tests Type for id. """
-        
+
         with self.assertRaises(TypeError):
             b2 = Base("Incorrect Type")
 
@@ -32,3 +32,37 @@ class TestBase(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             b3 = Base(1, 1)
+
+    def test_empty_to_json_string(self):
+        """ Test for passing empty list. """
+
+        json_string = Base.to_json_string([])
+        self.assertTrue(type(json_string) is str)
+        self.assertEqual(json_string, "[]")
+
+    def test_None_to_json_String(self):
+        json_s = Base.to_json_string(None)
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
+
+    def test_from_json_string(self):
+        """Tests regular from_json_string"""
+        json_str = '[{"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}, \
+{"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}]'
+        json_l = Base.from_json_string(json_str)
+        self.assertTrue(type(json_l) is list)
+        self.assertEqual(len(json_l), 2)
+        self.assertTrue(type(json_l[0]) is dict)
+        self.assertTrue(type(json_l[1]) is dict)
+        self.assertEqual(json_l[0],
+                         {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8})
+        self.assertEqual(json_l[1],
+                         {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0})
+
+    def test_fjs_empty(self):
+        """Tests from_json_string with an empty string"""
+        self.assertEqual(Base.from_json_string(""), [])
+
+    def test_fjs_None(self):
+        """Tests from_json_string with an empty string"""
+        self.assertEqual([], Base.from_json_string(None))
