a2de26e8f0624ed9cd42beaecfbb38737f191cb1
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-06 10:29:09 +0300

    1 - 3 Languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const firstVar = 'C is fun';
+const secondVar = 'Python is cool';
+const thirdVar = 'JavaScript is amazing';
+console.log(firstVar);
+console.log(secondVar);
+console.log(thirdVar);
