88d62a4bd02490f2675fd9653e96ec2ad527f233
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-06 13:25:31 +0300

    0 output that is an integer

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const myArray = process.argv.slice(2);
 const arrayLength = myArray.length;
 
 if (arrayLength < 2) {
-  console.log('0');
+  console.log(0);
 } else {
   myArray.sort();
   console.log(myArray[myArray.length - 2]);
