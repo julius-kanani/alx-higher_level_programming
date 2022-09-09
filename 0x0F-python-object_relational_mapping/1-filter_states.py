a429d8c2c3533cdccc0e3a1bf0c573d318fe1c29
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-09 17:02:20 +0300

    1 - lists all states with a name starting with N (uppercase N)

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 0-select_states module. This module lists all states from the database
+hbtn_0e_0_usa. """
+
+
+import MySQLdb
+from sys import argv
+
+
+def filter_states():
+    """ Lists all states with a name starting with N from the database
+        hbtn_0e_0_usa. """
+
+    db = MySQLdb.connect(
+            host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    result = cursor.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    filter_states()
