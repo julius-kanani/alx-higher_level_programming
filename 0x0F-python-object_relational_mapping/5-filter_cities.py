048da09d0c4e75d0c6cf41100065c33223c18cee
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-10 14:51:57 +0300

    5 - All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" 5-filter_cities module. This module takes in the name of a state as an
+    argument and lists all cities of that state, using the database
+    hbtn_0e_4_usa. """
+
+
+import MySQLdb
+from sys import argv
+
+
+def filter_states():
+    """ Lists all cities of a given state from the database
+        hbtn_0e_4_usa. """
+
+    db = MySQLdb.connect(
+            host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute(
+            "SELECT c.name \
+                FROM cities as c \
+                    JOIN states as s \
+                        ON c.state_id = s.id \
+             WHERE s.name = %s \
+             ORDER BY c.id ASC;", (argv[4],)
+            )
+
+    rows = cursor.fetchall()
+    for row in rows:
+        if row == rows[-1]:
+            print(row[0])
+            break
+        print(row[0], end=", ")
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    filter_states()
