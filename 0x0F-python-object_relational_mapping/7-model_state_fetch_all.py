5d57a0d1264b1a0a9b1d02573e833cd930a04071
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-10 16:33:17 +0300

    7 - All states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" 7-model_state_fetch_all module. Lists all State objects from the
+    hbtn_0e_6_usa database. """
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def model_state_fetch_all(username=argv[1], passwd=argv[2], db=argv[3]):
+    """ Lists all State objects from the database hbtn_0e_6_usa.
+
+    Args:
+        username (str): MySQL username.
+        passwd (str): MySQL passwd.
+        db (str): The database name.
+
+    Returns:
+        State objects from the database hbtn_0e_6_usa.
+    """
+
+    # initialize engine
+    engine = engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(username, passwd, db),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # Initialize session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the hbtn_0e_6_usa
+    query = session.query(State).order_by(State.id).all()
+
+    # Print out the result set
+    for record in query:
+        print("{}: {}".format(record.id, record.name))
+
+    # Close the connection
+    session.close()
+
+
+if __name__ == "__main__":
+    model_state_fetch_all()
