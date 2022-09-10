d37d81d04f41c39a8c9f7caf9a0dee0f4b7bc98e
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-10 14:26:29 +0300

    4 - Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" 0-select_states module. This module lists all states from the database
-hbtn_0e_0_usa. """
+""" 4-cities_by_state module. This module lists all cities from the database
+hbtn_0e_4_usa. """
 
 
 import MySQLdb
@@ -8,7 +8,7 @@ from sys import argv
 
 
 def list_states():
-    """ Lists all states from the database hbtn_0e_0_usa. """
+    """ Lists all cities from the database hbtn_0e_4_usa. """
 
     db = MySQLdb.connect(
             host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
