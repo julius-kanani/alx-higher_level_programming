184c73bca221965da1bd3e9d361f653c4206ca00
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-11 18:57:07 +0300

    14 - model_city module, contains the class definition of a City

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" The model_city module. Cities in state model.
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from model_state import Base, State
+
+
+class City(Base):
+    """ Defines the City Model
+
+        Attributes:
+            id (int): The class unique id, that is auto-generated.
+            name (str): The name of the city.
+    """
+    __tablename__ = "city"
+
+    id = Column(
+            Integer, primary_key=True, unique=True, nullable=False,
+            autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
+
+    def __repr__(self):
+        """ Returns a string representation instance of the city. """
+
+        return "City(id={:d}, name={:s} state_id={:d})".format(
+                self.id, self.name, self.state_id)
