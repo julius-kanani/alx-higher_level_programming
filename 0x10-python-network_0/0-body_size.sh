676cfe551982cdc14eb115654937c3f3548a1fab
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-23 10:29:50 +0300

    0 - bash script to be 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-
+# Takes in a URL, sends request to URL, displays size of body of response.
 curl -Is "$1" | grep "Content-Length" | cut -d " " -f 2
