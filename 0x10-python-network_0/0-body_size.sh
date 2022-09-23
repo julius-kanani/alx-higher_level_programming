f1bece972e83fe1e27bdf6b2d4342184b1687362
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-23 10:25:51 +0300

    0 - removed extra comment on line 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the size of the
-# body of the response.
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response.
 
 curl -Is "$1" | grep "Content-Length" | cut -d " " -f 2
