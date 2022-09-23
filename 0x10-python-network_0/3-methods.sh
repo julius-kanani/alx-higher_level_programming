36d7e15db5c5d9aec37603256c3d239b60b7df02
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-23 11:37:21 +0300

    3 - HTTp headers only allowed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in URL, displays all HTTP methods the server will accept.
-curl -sIX OPTIONS "$1" | grep "Allow" | cut -d " " -f 2
+curl -sI "$1" | grep "Allow:" | cut -d " " -f 2
