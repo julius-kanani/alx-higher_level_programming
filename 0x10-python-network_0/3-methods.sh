82e283aefb257ce6fafea0018c68ef106c2cdbf2
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-23 12:22:56 +0300

    3 - displays all http methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in URL, displays all HTTP methods the server will accept.
-curl -sIX OPTIONS 127.0.0.1:80 | grep "Allow" | cut -d " " -f2 | sed 's/,/, /g'
+curl -sI -X OPTIONS "$1" | grep "Allow" | cut -d " " -f2 | sed 's/,/, /g'
