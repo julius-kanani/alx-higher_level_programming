b710c4488cbf4f7a6e4320850bef4d3d695f426a
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-28 22:06:26 +0300

    0 - fetches data from a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 0-hbtn_status module. """
+import urllib.request
+
+
+if __name__ == "__main__":
+    ext_url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(ext_url) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
