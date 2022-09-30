596256abc346c0d6cec6aa8c035157c16a5e5332
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-30 12:26:10 +0300

    6 - send a post request using requests module

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin.python3
+""" 6-post_email module. Takes in a URL and an email address, sends a
+    POST request to the passed URL with the email as a parameter,
+    and finally displays the body of the response. """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # Get user email and the URL to visit.
+    url, email = sys.argv[1], sys.argv[2]
+
+    # Send a POST request
+    result = requests.get(url, data={'email': email})
+
+    # Display the body of the response
+    print(result.txt)
