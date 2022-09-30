103a4372092a28ebd6eae0d7fe924f19ebd6b96b
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-30 12:02:25 +0300

    4 - fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 4-hbtn_status module. fetches https://alx-intranet.hbtn.io/status. """
+import requests
+
+
+if __name__ == "__main__":
+    result = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response: ")
+    print("\t- type: <class 'str'>")
+    print("\t- content: {:s}".format(result.text))
