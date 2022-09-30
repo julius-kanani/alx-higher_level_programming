d35ab6adf7a0d4364c58cc8444f3a5f5c6ba6cb3
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-30 11:32:29 +0300

    3 - Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 3-error_code module. takes in a URL, sends a request to the URL,
+    and displays the body of the response (decoded in utf-8). """
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            result = response.read()
+    except urllib.error.HTTPError as Error:
+        print("Error code: {:d}".format(Error.code()))
