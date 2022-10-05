5714ab49eb9508b6acdafd8f3145354984e7b521
Julius Kanani <juliusm.kanani@gmail.com>
2022-10-05 11:53:53 +0000

    10 - lists the first 10 commits of a repository

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 100-github_commits module. Takes two arguments.
+    Lists 10 commists (from the most recent to oldest of the a repository.
+    The first argument is the repository name.
+    The second argument is the owner name of the repository. """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo_name, owner_name = sys.argv[1], sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            owner_name, repo_name)
+    response = requests.get(url)
+    commits = response.json()
+    for i in range(10):
+        print("{}: {}".format(
+              commits[i].get('sha'),
+              commits[i].get('commit').get('author').get('name')))
