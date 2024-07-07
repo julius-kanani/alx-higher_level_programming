c732bc4be94f8eaa4121946d11c89982317d0be1
julius-kanani <juliusm.kanani@gmail.com>
2024-07-07 17:46:16 +0000

    added 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send request to the provided URL and display only the status code
-curl -s -I -o /dev/null -w "%{http_code}" "$1"
+# Display only the status code of the response for a given URL.
+curl -s -o /dev/null -w "%{http_code}" "$1"
