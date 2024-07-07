26c629c28a06c05e6cb3e3cafb2ff5af1fee9826
julius-kanani <juliusm.kanani@gmail.com>
2024-07-07 18:15:24 +0000

    100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send request and display status code
-curl -s -o /dev/null -w "%{http_code}\n" "$1"
+# This script sends a request to a URL passed as an argument and displays only the status code of the response
+curl -s -o /dev/null -w "%{http_code}" "$1"
